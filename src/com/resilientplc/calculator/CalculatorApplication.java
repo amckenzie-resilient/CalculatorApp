21a1aab6d842fb3f15b65280400b186947260403
Andrae McKenzie <teddybearzero1@gmail.com>
2019-01-17 16:53:11 +0000

    Added a simple error check of the value entered by the user

--- src/com/resilientplc/calculator/CalculatorApplication.java
+++ src/com/resilientplc/calculator/CalculatorApplication.java
@@ -1,4 +1,5 @@
 package com.resilientplc.calculator;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CalculatorApplication {
@@ -6,7 +7,7 @@ public class CalculatorApplication {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        int val1, val2, result;
+        int val1 = 0, val2 = 0, result;
 
         int operation;  // capture operation input from the user
         Calculator cal = new Calculator();  //  a new calculator object for calculations
@@ -15,15 +16,30 @@ public class CalculatorApplication {
 
         while (open) {
 
-            System.out.print("\nPlease enter your First number: ");
-            val1 = input.nextInt();
+            // Enter first number
+            try {
+                System.out.print("\nPlease enter your First number: ");
+                val1 = input.nextInt();
+            }
+            catch (InputMismatchException e ){
+                System.out.print("\nPlease enter a valid number!!!\n");
+                input.nextLine();
+            }
 
+            // Select operation type
             System.out.print("Please enter a valid operation: \n");
             System.out.print(" 1: Addition: \n 2: Subtract: \n 3: Multiplication: \n 4: Division: \n");
             operation = input.nextInt();
 
-            System.out.print("Please enter your Second number: ");
-            val2 = input.nextInt();
+            // Enter second number
+            try {
+                System.out.print("Please enter your Second number: ");
+                val2 = input.nextInt();
+            }
+            catch (InputMismatchException e){
+                System.out.print("Please enter a valid number!!!\n");
+                input.nextLine();
+            }
 
             switch (operation) {
                 case 1:
@@ -48,11 +64,17 @@ public class CalculatorApplication {
                     break;
             }
 
-            //Display the first and second number values entered by the user
-            System.out.print("First Number : " + val1 + " \nSecond Number : " + val2);
+            //Display the first number values entered by the user
+            System.out.print("First Number : " + val1 + "\n");
+
+            // Display operation
+            System.out.print("Operation : " + operation + "\n");
+
+            System.out.print("Second Number : " + val2 + "\n");
 
             // Display the Result
             System.out.print("\nResult : " + " " + val1 + " " + operation + " " + val2 + " = " + result + "\n");
+
         }
     }
 
