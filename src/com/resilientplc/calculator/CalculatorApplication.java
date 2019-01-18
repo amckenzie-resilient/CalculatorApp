76d27b0d285deb422e97f7519dc2280ae29ed987
amckenzie-resilient <a.mckenzie@resilientplc.com>
2019-01-18 10:40:10 +0000

    Updated the error handling for value input

--- src/com/resilientplc/calculator/CalculatorApplication.java
+++ src/com/resilientplc/calculator/CalculatorApplication.java
@@ -9,67 +9,60 @@ public class CalculatorApplication {
         Scanner input = new Scanner(System.in);
         int val1 = 0, val2 = 0, result = 0;
 
-        String operation;                       // capture operation input from the user
-        Calculator cal = new Calculator();      //  a new calculator object for calculations
+        String operation;                           // capture operation input from the user
+        Calculator cal = new Calculator();          //  a new calculator object for calculations
 
-        boolean open = true;                    // while the app is open
+        boolean open = true;                        // while the app is open
 
         while (open) {
 
-            // Enter first number
             try {
+
                 System.out.print("\nPlease enter your First number: ");
                 val1 = input.nextInt();
-            }
-            catch (InputMismatchException e ){
-                System.out.print("\nPlease enter a valid number!!!\n");
-                input.nextLine();
-            }
 
-            // Select operation type
-            System.out.print("Please enter a valid operation: ");
-            operation = input.next();
+                System.out.print("Please enter a valid operation: ");
+                operation = input.next();
 
-            // Enter second number
-            try {
                 System.out.print("Please enter your Second number: ");
                 val2 = input.nextInt();
+
+                switch (operation) {
+                    case "+":
+                        result = cal.add(val1, val2);
+                        break;
+                    case "-":
+                        result = cal.subtract(val1, val2);
+                        break;
+                    case "*":
+                        result = cal.multiple(val1, val2);
+                        break;
+                    case "/":
+                        result = cal.divide(val1, val2);
+                        break;
+                    default:
+                        System.out.print("Invalid Operation Selected");
+                        break;
+                }
+
+                //Display the first number value entered by the user
+                System.out.print("\nFirst Number : " + val1 + "\n");
+
+                // Display operation
+                System.out.print("Operation : " + operation + "\n");
+
+                //Display the second number value entered by the user
+                System.out.print("Second Number : " + val2 + "\n");
+
+                // Display the Result
+                System.out.print("\nResult : " + " " + val1 + " " + operation + " " + val2 + " = " + result + "\n");
+
             }
+
             catch (InputMismatchException e){
                 System.out.print("Please enter a valid number!!!\n");
                 input.nextLine();
             }
-
-            switch (operation) {
-                case "+":
-                    result = cal.add(val1, val2);
-                    break;
-                case "-":
-                    result = cal.subtract(val1, val2);
-                    break;
-                case "*":
-                    result = cal.multiple(val1, val2);
-                    break;
-                case "/":
-                    result = cal.divide(val1, val2);
-                    break;
-                default:
-                    //result = 0;
-                    System.out.print("Invalid Operation Selected");
-                    break;
-            }
-
-            //Display the first number values entered by the user
-            System.out.print("\nFirst Number : " + val1 + "\n");
-
-            // Display operation
-            System.out.print("Operation : " + operation + "\n");
-
-            System.out.print("Second Number : " + val2 + "\n");
-
-            // Display the Result
-            System.out.print("\nResult : " + " " + val1 + " " + operation + " " + val2 + " = " + result + "\n");
-
         }
     }
 
