7c1a37e8b8a17472fbe4a67fab686e58a415bdfc
amckenzie-resilient <a.mckenzie@resilientplc.com>
2019-01-22 11:54:40 +0000

    Can now exit and enter the help menu by using commands

--- src/com/resilientplc/calculator/CalculatorApplication.java
+++ src/com/resilientplc/calculator/CalculatorApplication.java
@@ -11,40 +11,44 @@ public class CalculatorApplication {
         String help;
 
         int val1 = 0, val2 = 0, result = 0;
+        String menuOption;
         String fullExpression;
 
-        String operation;                           // capture operation input from the
+        String operation = "";                      // capture operation input from the
         Calculator cal = new Calculator();          // a new calculator object for calculations
-        boolean open = true;                        // while the app is open
+        boolean mainCalculator = true;              // while the app is open
+        boolean helpMenu = false;
 
-        String menuSwitch;
+        while (mainCalculator) {
 
-        while (open) {
+            menu.CreateMenu();
 
             try {
 
-                menuSwitch = input.next();
+                System.out.print("\nPlease enter your First number: ");
 
-                switch (menuSwitch) {
-                    case "help":
-                        menu.Help();
-                        break;
-                    case "exit":
-                        menu.Exit();
-                        break;
+                if (input.hasNext("help")){
+                    mainCalculator = false;
+                    helpMenu = true;
+                    break;
+                }else if (input.hasNext("exit")){
+                    mainCalculator = false;
+                    break;
+                }else {
+                    val1 = input.nextInt();
                 }
 
-                menu.CreateMenu();
-
-                System.out.print("\nPlease enter your First number: ");
-                val1 = input.nextInt();
-
                 System.out.print("Please enter a valid operation: ");
-                operation = input.next();
-
-                System.out.print("Please enter your Second number: ");
-                val2 = input.nextInt();
-
+                if (input.hasNext("help")) {
+                    mainCalculator = false;
+                    helpMenu = true;
+                    break;
+                }else if (input.hasNext("exit")){
+                    mainCalculator = false;
+                    break;
+                } else {
+                    operation = input.next();
+                }
 
                 switch (operation) {
                     case "+":
@@ -56,39 +60,62 @@ public class CalculatorApplication {
                     case "*":
                         result = cal.multiple(val1, val2);
                         break;
-                    case "/":
+                     case "/":
                         result = cal.divide(val1, val2);
                         break;
-                    default:
+                     default:
                         System.out.print("Invalid Operation Selected");
                         break;
+                    }
+
+               System.out.print("Please enter your Second number: ");
+
+                if (input.hasNext("help")){
+                    mainCalculator = false;
+                    helpMenu = true;
+                    break;
+                }else if (input.hasNext("exit")){
+                    mainCalculator = false;
+                    break;
+                }else {
+                    val2 = input.nextInt();
                 }
 
-                //Display the first number value entered by the user
-                System.out.print("\nFirst Number : " + val1 + "\n");
+               //Display the first number value entered by the user
+               System.out.print("\nFirst Number : " + val1 + "\n");
 
-                // Display operation
-                System.out.print("Operation : " + operation + "\n");
+               // Display operation
+               System.out.print("Operation : " + operation + "\n");
 
-                //Display the second number value entered by the user
-                System.out.print("Second Number : " + val2 + "\n");
+               //Display the second number value entered by the user
+               System.out.print("Second Number : " + val2 + "\n");
 
-                // Display the Result
-                System.out.print("\nResult : " + " " + val1 + " " + operation + " " + val2 + " = " + result + "\n");
+               // Display the Result
+               System.out.print("\nResult : " + " " + val1 + " " + operation + " " + val2 + " = " + result + "\n");
 
-                // Enter a full Expression Task 2
-                System.out.print("\n***************** Task 2 **********************");
 
-                System.out.print("\nPlease Enter a full expression\n");
-                fullExpression = input.next();  // need to change to capture the whole string and pass to calculator class
-                cal.expression(fullExpression);
+               // Enter a full Expression Task 2
+               System.out.print("\n***************** Task 2 **********************");
+
+               System.out.print("\nPlease Enter a full expression\n");
+               fullExpression = input.next();  // need to change to capture the whole string and pass to calculator class
+               cal.expression(fullExpression);
 
             }
 
-            catch (InputMismatchException e){
-                System.out.print("Please enter a valid number!!!\n");
-                input.next();
-            }
+           catch (InputMismatchException e){
+
+               System.out.print("Please enter a valid number!!!\n");
+               input.next();
+           }
+
+        }
+
+        while (helpMenu){
+
+            mainCalculator = true;
+            menu.Help();
+            menuOption = input.next();
 
         }
     }
