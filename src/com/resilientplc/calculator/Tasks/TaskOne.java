496f08a4237aaa9c4c6a6236487c0642ada2aa6a
amckenzie-resilient <a.mckenzie@resilientplc.com>
2019-01-30 14:12:10 +0000

    Small changes to Task One class

--- src/com/resilientplc/calculator/Tasks/TaskOne.java
+++ src/com/resilientplc/calculator/Tasks/TaskOne.java
@@ -1,7 +1,6 @@
 package com.resilientplc.calculator.Tasks;
 
 import com.resilientplc.calculator.Calculations.Calculator;
-import com.resilientplc.calculator.ExceptionHandling.NumberInputException;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -22,31 +21,38 @@ public class TaskOne extends CalculatorApp {
         System.out.print("\n***************** Task 1 **********************\n");
 
         System.out.print("\nPlease enter your First number: ");
-        setFirstNumber();
 
-        System.out.print("Please enter a valid operator: ");
-        setOperator();
+        try {
+            setFirstNumber();
+            System.out.print("Please enter a valid operator: ");
+            setOperator();
 
-        System.out.print("Please enter your Second number: ");
-        setSecondNumber();
+            System.out.print("Please enter your Second number: ");
+            setSecondNumber();
 
-        System.out.print("First Number : " + getFirstNumber() + "\n");
-        System.out.print("Operator : " + getOperator() + "\n");
-        System.out.print("Second Number : " + getSecondNumber() + "\n" + "\n");
+            System.out.print("First Number : " + getFirstNumber() + "\n");
+            System.out.print("Operator : " + getOperator() + "\n");
+            System.out.print("Second Number : " + getSecondNumber() + "\n" + "\n");
 
-        result = getResult(getFirstNumber(), getOperator(), getSecondNumber());
+            result = getResult(getFirstNumber(), getOperator(), getSecondNumber());
 
-        // Display the Result
-        System.out.print("Result : " + " " + getFirstNumber() + " " + getOperator() + " " + getSecondNumber() + " = " + result + "\n");
+            // Display the Result
+            System.out.print("Result : " + " " + getFirstNumber() + " " + getOperator() + " " + getSecondNumber() + " = " + result + "\n");
+
+        }catch ( InputMismatchException e){
+            System.out.print("Not a number, please try again!");
+        }catch ( NumberFormatException e){
+            System.out.print("Not a valid operator, please try again!");
+        }
 
     }
 
-    private void setFirstNumber()
+    private void setFirstNumber() throws InputMismatchException
     {
-      firstNumber = input.nextInt();
+        firstNumber = input.nextInt();
     }
 
-    private void setSecondNumber()
+    private void setSecondNumber() throws InputMismatchException
     {
       secondNumber = input.nextInt();
     }
@@ -90,14 +96,4 @@ public class TaskOne extends CalculatorApp {
 
         return result;
     }
-
-    public int checkNumberInput(int val) throws InputMismatchException {
-
-        if (val != (int) val) {
-
-            throw new InputMismatchException("Please enter a valid number" + val);
-        }
-        return val;
-    }
-
 }
